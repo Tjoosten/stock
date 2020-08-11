7185c02e56393e0e406a06f5e730ad99051658a4
Tim Joosten <Tjoosten@users.noreply.github.com>
2020-08-11 04:51:17 +0200

    Delete _form.blade.php

--- app/views/search/_form.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-10">
-        {{ Form::bt_text('search', null, null, array('class' => 'input-lg', 'placeholder' => 'Search', 'tabindex' => '1')) }}
-    </div>
-    <div class="col-md-2">
-        {{ Form::bt_button('Zoek', array('class' => 'btn-pack btn-block btn-lg', 'tabindex' => '2')) }}
-    </div>
-</div>
