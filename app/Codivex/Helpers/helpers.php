a4a419f2acc637292421e5e349f781ff32a1e30c
Tim Joosten <Tjoosten@users.noreply.github.com>
2020-08-11 04:20:43 +0200

    Delete helpers.php

--- app/Codivex/Helpers/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function sort_by($route, $column, $body)
-{
-    $direction = (Request::get('direction') == 'asc')? 'desc': 'asc';
-    return link_to_action($route, $body, ['sortBy' => $column, 'direction' => $direction]);
-}
